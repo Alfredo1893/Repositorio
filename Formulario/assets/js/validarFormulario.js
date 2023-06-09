6863da08a6811653e7b0605ace643f4621511ee5
roberto-guzman <r_a_mg@hotmail.com>
2023-06-09 16:22:21 -0600

    publish

--- Formulario/assets/js/validarFormulario.js
+++ Formulario/assets/js/validarFormulario.js
@@ -1,43 +1,51 @@
-function validarFormulario(event) {
-    event.preventDefault();
-  
-    var codigo = document.getElementById("codigo").value;
-    var descripcion = document.getElementById("descripcion").value;
-    var codigo_barra = document.getElementById("Codigo_barra").value;
-    var precio = document.getElementById("precio").value;
-    var stock = document.getElementById("stock").value;
-    var proveedor = document.getElementById("proveedor").value;
-    var estado = document.getElementById("estado").value;
-    var categoria = document.getElementById("categoria").value;
-  
-    switch (true) {
-        case codigo.trim() === "":
-          alert("¡Por favor, ingrese el codigo!");
-          break;
-        case descripcion.trim() === "":
-          alert("¡Por favor, ingrese la descripción!");
-          break; 
-        case codigo_barra.trim() === "":
-          alert("¡Por favor, ingrese el codigo de barra!");
-          break;          
-        case precio.trim() === "":
-          alert("¡Por favor, ingrese el precio!");
-          break;
-        case stock.trim() === "":
-          alert("¡Por favor, ingrese el stock del producto!");
-          break;
-        case proveedor.trim() === "":
-          alert("¡Por favor, ingrese el nombre de los proveedores!");
-          break;
-        case estado.trim() === "":
-          alert("¡Por favor, valide el estado!");
-          break;          
-        case categoria.trim() === "":
-          alert("¡Por favor, ingrese la categoria!");
-          break;
-        default:
-          window.location.href = "confirmacion.html";
-          break;
-      }
+// Esperando a que se envíe el formulario
+$('.needs-validation').on('submit', function(event) {
+  event.preventDefault();
+  validarFormulario();
+});
+
+function validarFormulario() {
+  var codigo = $('#codigo').val();
+  var descripcion = $('#descripcion').val();
+  var codigo_barra = $('#codigo_barra').val();
+  var precio = $('#precio').val();
+  var stock = $('#stock').val();
+  var proveedor = $('#proveedor').val();
+  var categoria = $('#categoria').val();
+
+  switch (true) {
+    case !codigo:
+      mostrarAlerta('Falta por rellenar el campo "Código"');
+      break;
+    case !descripcion:
+      mostrarAlerta('Falta por rellenar el campo "Descripción"');
+      break;
+    case !codigo_barra:
+      mostrarAlerta('Falta por rellenar el campo "Código de barra"');
+      break;
+    case !precio:
+      mostrarAlerta('Falta por rellenar el campo "Precio Unitario"');
+      break;
+    case !stock:
+      mostrarAlerta('Falta por rellenar el campo "Stock"');
+      break;
+    case !proveedor:
+      mostrarAlerta('Falta por rellenar el campo "Proveedor"');
+      break;
+    case !categoria:
+      mostrarAlerta('Falta por seleccionar una categoría');
+      break;
+    default:
+      mostrarConfirmacion();
+      break;
   }
-  
+}
+
+function mostrarAlerta(mensaje) {
+  $('.needs-validation').addClass('was-validated');
+  alert(mensaje);
+}
+
+function mostrarConfirmacion() {
+  window.location.href = "confirmacion.html";
+}
